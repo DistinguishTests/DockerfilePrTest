03a2482fe94f153e85a9ddf775e9d9444599db19
Ron-Amihai <ron.amihai@cycode.com>
2022-12-08 09:01:14 +0200

    Added files to check IaC graph model creation

---
+++ vars2.tf
@@ -0,0 +1,14 @@
+variable "enforce_ssl_vars" {
+  type    = bool
+  default = false
+}
+
+variable "hosted_zone_id" {
+  type    = string
+  default = "Z2O1EMRO9K5GLX3"
+}
+
+variable "mfa_enabled" {
+  type    = bool
+  default = false
+}
