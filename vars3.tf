b8f42af2bf382117256f3314f04d0b05f49428c1
Ron-Amihai <ron.amihai@cycode.com>
2023-08-20 10:54:19 +0300

    Added File

---
+++ vars3.tf
@@ -0,0 +1,16 @@
+variable "enforce_ssl_vars" {
+  type    = "SDFSDFSDF"
+  default = false
+}
+
+http://ron:samp222@google.com
+
+variable "hosted_zone_id" {
+  type  = "http://ron:samp333@google.com"
+  default = "Z2O1EMRO9K5GLX3"
+}
+
+variable "mfa_enabled" {
+  type    = bool
+  default = false
+}
