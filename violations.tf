af90d5ba3b73977de4c5da3c9691804868a12cd6
Ron-Amihai <ron.amihai@cycode.com>
2023-08-27 11:58:25 +0300

    Staging PR

--- violations.tf
+++ violations.tf
@@ -2,7 +2,7 @@ resource "azurerm_postgresql_server" "example_ssl_enforcement_enabledOmerTest" {
     name                                = "example-psqlserver4"
     location                            = azurerm_resource_group.example.location
     resource_group_name                 = azurerm_resource_group.example.name
-    administrator_login                 = "psqladminun"
+    administrator_login                 = "625563426333-vlg5ga2tnv08820fddnbppcpkm4l7nbp.apps.googleusercontent.com"
     administrator_login_password        = "H@Sh1CoR3!"
     sku_name                            = "GP_Gen5_4"
     storage_mb                          = 640000
